b2cc6ec6a58ceca1b538be5a7f8ee7bfce53392a
JastrzabJan <jastrzab.janek@gmail.com>
2019-01-30 22:08:17 +0100

    Controllers

---
+++ Football_League_Site/Football_League_Site/Controllers/ContentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.WebApi;
+using Umbraco.Core;
+
+namespace Football_League_Site.Controllers
+{
+    public class ContentController : UmbracoApiController
+    {
+
+        public Boolean testAction()
+        {
+            return true;
+        }
+    }
+}   
---
+++ Football_League_Site/Football_League_Site/Controllers/PostNodesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Mvc;
+using Umbraco.Core;
+
+namespace Football_League_Site.Controllers
+{
+    public class PostNodesController : Controller
+    {
+        [HttpGet]
+        [ActionName("createTestNode")]
+        public bool createNewNode()
+        {
+            var contentService = ApplicationContext.Current.Services.ContentService;
+            var publishDate = DateTime.Now;
+            var content = contentService.CreateContent("test13", 1006, "Conent Page");
+            content.ReleaseDate = publishDate;
+            content.UpdateDate = publishDate;
+            content.CreateDate = publishDate;
+
+            content.SetValue("pageTitle", "test13");
+            //content.SetValue("Name", "test13");
+
+            var result = contentService.SaveAndPublishWithStatus(content);
+
+            return result;
+        }
+
+        [HttpGet]
+        [ActionName("testController")]
+        public void testController()
+        {
+
+        }
+    }
+}
